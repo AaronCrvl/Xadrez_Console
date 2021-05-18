f6fa19c9f9ec89ae5afed5dbfdac6a9661542bba
Aaron Carvalho <carvalhosins@gmail.com>
2021-05-18 20:32:00 -0300

    Criando classe de posição do Tabuleiro

---
+++ Xadrez_Console/Posicao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+       //Dados:
+        public int linha { get; set;  }
+        public int coluna { get; set; }
+
+        //Construtor
+        Posicao(int _linha, int _coluna ) 
+        {
+            this.linha = _linha;
+            this.coluna = _coluna;
+        }
+
+        //Métodos:
+        public override string ToString()
+        {
+            // Quando objeto da classe é criado e jogado para o output é tratado da forma abaixo:
+            return this.linha
+                + ","
+                + this.coluna;
+        }
+    }
+}
--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Xadrez_Console
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
         }
     }
 }
