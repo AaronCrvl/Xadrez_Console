30c9fd991ea696386b06d67ba015fe2c384eae7c
Aaron Carvalho <carvalhosins@gmail.com>
2021-05-18 21:07:20 -0300

    Início da Implementação de tabuleiro

---
+++ Xadrez_Console/Tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+
+        Peca(Posicao _pos, Cor _cor, Tabuleiro _tab ) 
+        {
+            this.posicao = _pos;
+            this.cor = _cor;
+            this.tab = _tab;
+            this.qtdMovimentos = 0;
+        }
+    }
+}
---
+++ Xadrez_Console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int _linhas, int _colunas)
+        {
+            this.linhas = _linhas;
+            this.colunas = _colunas;
+            pecas = new Peca[this.linhas, this.colunas];
+        }
+    }
+}
